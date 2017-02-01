9ed1607c17bd971efab3e593676d96a51d45ff91
Golois Mouelet <mgolois@yahoo.com>
2017-02-01 13:35:11 -0500

    update index html amd deleted bower component folder

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,69 +1,75 @@
 /*
  * Change Navbar color while scrolling
-*/
+ */
 
-$(window).scroll(function(){
-	handleTopNavAnimation();
+$(window).scroll(function() {
+    handleTopNavAnimation();
 });
 
-$(window).load(function(){
-	handleTopNavAnimation();
+$(window).load(function() {
+    handleTopNavAnimation();
 });
 
 function handleTopNavAnimation() {
-	var top=$(window).scrollTop();
+    var top = $(window).scrollTop();
 
-	if(top>10){
-		$('#site-nav').addClass('navbar-solid'); 
-	}
-	else{
-		$('#site-nav').removeClass('navbar-solid'); 
-	}
+    if (top > 10) {
+        $('#site-nav').addClass('navbar-solid');
+    } else {
+        $('#site-nav').removeClass('navbar-solid');
+    }
 }
 
 /*
  * Registration Form
-*/
+ */
 
-$('#registration-form').submit(function(e){
+$('#registration-form').submit(function(e) {
     e.preventDefault();
-    
+
     var postForm = { //Fetch form data
-            'fname'     : $('#registration-form #fname').val(),
-            'lname'     : $('#registration-form #lname').val(),
-            'email'     : $('#registration-form #email').val(),
-            'cell'      : $('#registration-form #cell').val(),
-            'address'   : $('#registration-form #address').val(),
-            'zip'       : $('#registration-form #zip').val(),
-            'city'      : $('#registration-form #city').val(),
-            'program'   : $('#registration-form #program').val()
+        'EventName': 'MenConferenceFeb2017',
+        'FirstName': $('#registration-form #fname').val(),
+        'LastName': $('#registration-form #lname').val(),
+        'EmailAddress': $('#registration-form #email').val(),
+        'Phone': $('#registration-form #cell').val(),
+        'NumberOfAdults': $('#registration-form #adults').val(),
+        'NumberOfChildren': $('#registration-form #children').val(),
+        'Address': $('#registration-form #address').val(),
+        'State': $('#registration-form #state').val(),
+        'City': $('#registration-form #city').val(),
+        'Country': $('#registration-form #country').val(),
+        'Transportation': $('#registration-form #transportation').val(),
+        'ChurchName': $('#registration-form #churchName').val(),
+        'Comments': $('#registration-form #comment').val()
     };
 
     $.ajax({
-            type      : 'POST',
-            url       : './assets/php/contact.php',
-            data      : postForm,
-            dataType  : 'json',
-            success   : function(data) {
-                            if (data.success) {
-                                $('#registration-msg .alert').html("Registration Successful");
-                                $('#registration-msg .alert').removeClass("alert-danger");
-                                $('#registration-msg .alert').addClass("alert-success");
-                                $('#registration-msg').show();
-                            }
-                            else
-                            {
-                                $('#registration-msg .alert').html("Registration Failed");
-                                $('#registration-msg .alert').removeClass("alert-success");
-                                $('#registration-msg .alert').addClass("alert-danger");
-                                $('#registration-msg').show();
-                            }
-                        }
-        });
+        type: 'POST',
+        url: 'https://cmfi-conference.azurewebsites.net/api/PostRegistration',
+        data: postForm,
+        contentType: 'application/json',
+        success: function(data) {
+            $('#registration-msg .alert').html("Registration Successful. Thank you for registering");
+            $('#registration-msg .alert').removeClass("alert-danger");
+            $('#registration-msg .alert').addClass("alert-success");
+            $('#registration-msg').show();
+            $('#registration-form input').val('');
+            $('#registration-form textarea').val('');
+        },
+        error: function(error) {
+            {
+                $('#registration-msg .alert').html("Sorry an error occurred while submitting your resgistration!");
+                $('#registration-msg .alert').removeClass("alert-success");
+                $('#registration-msg .alert').addClass("alert-danger");
+                $('#registration-msg').show();
+            }
+        }
+    });
 });
 
 /*
  * SmoothScroll
-*/
+ */
 
 smoothScroll.init();
